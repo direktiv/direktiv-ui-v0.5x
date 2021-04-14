ebd5b983d0ce0b06af25361300b0e7dc92e963ad
James <james.murtagh@vorteil.io>
2021-04-14 11:25:58 +1000

    Better workflows layout

--- src/components/workflow-page/index.js
+++ src/components/workflow-page/index.js
@@ -8,24 +8,20 @@ export default function WorkflowPage() {
             <div className="flex-row" style={{ maxHeight: "64px" }}>
                 <Breadcrumbs />
             </div>
-            <div id="workflow-row-1" className="flex-row">
-                <div id="workflow-yaml-tile" className="neumorph">
-                    <div style={{  }}>
-                        <h3>YAML Editor</h3>
-                    </div>
-                </div>
-                <div style={{ marginRight: "10px", height: "max-content",  display: "flex", flexWrap: "wrap"}}>
-                    <div className="neumorph chart-box">
-                        <p>Chart 1</p>
-                    </div>
-                    <div className="neumorph chart-box">
-                        <p>Chart 1</p>
-                    </div>
-                </div>
-            </div>
-            <div id="workflow-row-2" className="flex-row">
-                <div id="workflow-graph-tile" className="neumorph">
+            <div id="workflows-page">
+                <div className="flex-child" style={{ minWidth: "800px" }}>
+                    <div className="neumorph" style={{ height: "100%" }}>
 
+                    </div>
+                </div>
+                <div className="flex-child" style={{ minWidth: "300px", maxWidth: "300px", alignSelf: "stretch", flexDirection: "column" }}>
+                    <div className="chart-tile neumorph"></div>
+                    <div className="chart-tile neumorph"></div>
+                </div>
+                <div className="flex-child" style={{ width: "500px"}}>
+                    <div className="neumorph" style={{ height: "" }}>
+
+                    </div>
                 </div>
             </div>
         </>
