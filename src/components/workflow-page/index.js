c397f3c9c921db6c40d191bfe37ae9c2ec9df987
James <james.murtagh@vorteil.io>
2021-04-14 14:50:48 +1000

    Style fixes

--- src/components/workflow-page/index.js
+++ src/components/workflow-page/index.js
@@ -11,6 +11,8 @@ import PencilSquare from 'react-bootstrap-icons/dist/icons/pencil-square'
 import PieChartFill from 'react-bootstrap-icons/dist/icons/pie-chart-fill'
 import CardList from 'react-bootstrap-icons/dist/icons/card-list'
 import PipFill from 'react-bootstrap-icons/dist/icons/pip-fill'
+import CircleFill from 'react-bootstrap-icons/dist/icons/circle-fill'
+import { Circle } from 'react-bootstrap-icons'
 
 export default function WorkflowPage() {
 
@@ -20,8 +22,8 @@ export default function WorkflowPage() {
                 <Breadcrumbs />
             </div>
             <div id="workflows-page">
-                <div className="flex-child" style={{ minWidth: "700px" }}>
-                    <div className="neumorph" style={{  height: "auto", display:"flex", flexDirection:"column", alignSelf:"stretch", minHeight:"500px" }}>
+                <div className="flex-child" style={{ width: "550px", maxHeight: "520px" }}>
+                    <div className="neumorph" style={{  height: "auto", display:"flex", flexDirection:"column", alignSelf:"stretch", minHeight:"300px", maxHeight: "500px", maxWidth: "1080px" }}>
                         <TileTitle name="Editor">
                             <PencilSquare />
                         </TileTitle>
@@ -38,7 +40,7 @@ export default function WorkflowPage() {
                         </SizeMe>
                     </div>
                 </div>
-                <div className="flex-child" style={{ minWidth: "300px", maxWidth: "300px", alignSelf: "stretch", flexDirection: "column" }}>
+                <div className="flex-child" style={{ minWidth: "300px", maxWidth: "300px", maxHeight: "520px", alignSelf: "stretch", flexDirection: "column" }}>
                     <div className="chart-tile neumorph">
                         <TileTitle name="Chart">
                             <PieChartFill />
@@ -55,7 +57,7 @@ export default function WorkflowPage() {
                         </div>
                     </div>
                 </div>
-                <div className="flex-child" style={{ width: "600px"}}>
+                <div className="flex-child" style={{ width: "600px", minHeight: "360px"}}>
                     <div className="neumorph" style={{ height: "auto", display:"flex", flexDirection:"column" }}>
                         <TileTitle name="Graph">
                             <PipFill />
@@ -85,9 +87,25 @@ function EventsList() {
 
     let listItems = [];
     for (let i = 0; i < lines.length; i++) {
+
+        let colorClass = "failed";
+        let z = i % 3;
+        switch (z) {
+            case 0:
+                colorClass = "failed";
+                break;
+            case 1:
+                colorClass = "pending";
+                break;
+            case 2:
+                colorClass = "success";
+                break;
+        }
+
         listItems.push(
             <li className="event-list-item">
                 <div>
+                    <span><CircleFill className={colorClass} style={{ paddingTop: "5px", marginRight: "4px", maxHeight: "8px" }} /></span>
                     <span style={{ fontSize: "8pt", textAlign: "left", marginRight: "10px" }}>
                         10m ago
                     </span>
