63d24f49b1278fb4fbe1268579b661301e61f29a
Jon Alfaro <jon.alfaro@vorteil.io>
2021-04-15 10:08:03 +1000

    editor: better padding and width resizing
    Signed-off-by: Jon Alfaro <jon.alfaro@vorteil.io>

--- src/components/workflow-page/index.js
+++ src/components/workflow-page/index.js
@@ -37,7 +37,7 @@ export default function WorkflowPage() {
                             <PencilSquare />
                         </TileTitle>
                         <div style={{ display: "flex", width: "100%", height: "100%", position: "relative", top: "-28px"}}>
-                            <div style={{width: "100%", paddingTop: "10px"}}>
+                            <div style={{width: "100%", paddingTop: "18px"}}>
                             <Editor/>
                             </div>
                         </div>
