01dee07c6170cebab211cfdf91e357015c6041c8
trent <trent.hilliam@vorteil.io>
2021-05-20 12:39:33 +1000

    Updated logs

--- src/components/instance-page/logs.js
+++ src/components/instance-page/logs.js
@@ -142,7 +142,7 @@ export default function Logs(props) {
     }, [checkScrollDirection])
 
     return(
-        <div className="editor-wrapper" style={{display: "flex", flexDirection: "row", flexWrap: "wrap", width: "100%", height: "100%", minHeight:"300px",  top:"-28px", position: "relative", boxShadow:"none"}}>
+        <div id="logs-toggle" className="editor-wrapper" style={{display: "flex", flexDirection: "row", flexWrap: "wrap", width: "100%", height: "100%", minHeight:"300px",  top:"-28px", position: "relative", boxShadow:"none"}}>
             <div style={{width: "100%", height: "100%"}}>
                 <div style={{background:"#2a2a2a", height:"100%", top: "28px", marginTop:"28px"}}>
                     <div id="logs" style={{borderRadius:"8px", overflow: tail ? "hidden":"auto", textAlign:"left", height: "auto", color:"white", fontSize:"12pt", padding:"5px", background:"#2a2a2a", position: "absolute", top:"28px", bottom:"30px", paddingBottom:"10px" }}>
