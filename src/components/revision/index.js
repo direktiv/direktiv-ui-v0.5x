687c2a572d8a1e3a95f5d94376ca970a7a4e79f0
trent <trent.hilliam@vorteil.io>
2021-08-20 12:19:49 +1000

    Updated ui to have a revision page

--- src/components/revision/index.js
+++ src/components/revision/index.js
@@ -4,9 +4,15 @@ import TileTitle from '../tile-title'
 import { useParams } from 'react-router'
 import LoadingWrapper from "../loading"
 import { CopyToClipboard } from "../../util-funcs"
+import CircleFill from 'react-bootstrap-icons/dist/icons/circle-fill'
 
 import { IoList, IoCopy, IoEyeOffSharp, IoEyeSharp} from 'react-icons/io5'
 import MainContext from '../../context'
+
+
+import * as dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 export default function Revision() {
     const {sse} = useContext(MainContext)
@@ -16,10 +22,11 @@ export default function Revision() {
     const [podSource, setPodSource] = useState(null)
     const [revisionSource, setRevisionSource] = useState(null)
 
+    const [revisionDetails, setRevisionDetails] = useState(null)
+
     const [pods, setPods] = useState([])
     const podsRef = useRef(pods)
     const [tab, setTab] = useState("")
-    const tabRef = useRef(tab)
 
     // set revision soruce
     useEffect(()=>{
@@ -36,7 +43,11 @@ export default function Revision() {
             }
 
             async function getData(e) {
-                console.log(e, "WATCH FOR REVISION BOIII")
+                let json = JSON.parse(e.data)
+
+                if (json.event === "ADDED") {
+                    setRevisionDetails(json.revision)
+                }
             }
 
             eventConnection.onmessage = e => getData(e)
@@ -62,7 +73,6 @@ export default function Revision() {
 
             async function getData(e) {
                 let pods = podsRef.current
-                let tabbed = tabRef.current
 
                 let json = JSON.parse(e.data)
                 switch (json.event) {
@@ -112,7 +122,8 @@ export default function Revision() {
         )
     }
 
-
+    console.log(revisionDetails, "REVISION")
+    console.log(pods, "PODS")
     return(
         <div className="container" style={{ flex: "auto", padding: "10px" }}>
             <div className="container">
@@ -125,7 +136,7 @@ export default function Revision() {
                     <IoList />
                 </TileTitle>
                 <LoadingWrapper isLoading={isLoading} text={"Loading Revision Details"}>
-
+                    { revisionDetails !== null ? <DetailedRevision serviceName={revision} pods={pods}  revision={revisionDetails} /> : "" }
                 </LoadingWrapper>
             </div>
             <div className="shadow-soft rounded tile" style={{ flex: 1, overflow:"hidden" }}>
@@ -135,6 +146,84 @@ export default function Revision() {
                 <LoadingWrapper isLoading={isLoading} text={"Loading Pod Logs"}>
                     <PodLogs sse={sse} pod={tab} service={service} namespace={namespace} revision={revision} />
                 </LoadingWrapper>
+            </div>
+        </div>
+    )
+}
+
+function DetailedRevision(props) {
+    const {revision, pods, serviceName} = props
+    let size = ""
+    if(revision.size === 0) {
+        size = "small"
+    } else if (revision.size === 1) {
+        size = "medium"
+    } else if (revision.size === 2) {
+        size = "large"
+    }
+    console.log(size)
+    return(
+        <div style={{fontSize:"11pt"}}>
+            <div style={{display:"flex", width:"100%"}}>
+                <div style={{flex: 1, textAlign:"left"}}>
+                    <p style={{margin:"5px"}}><div style={{width:"150px", display:"inline-block"}}><b>Created:</b></div>  {dayjs.unix(revision.created).format('h:mm a, DD-MM-YYYY')}</p>
+                    {size !== "" ? <p style={{margin:"5px"}}><div style={{width:"150px", display:"inline-block"}}><b>Size:</b></div> {size}</p> : ""}
+                    {revision.generation ? <p style={{margin:"5px"}}><div style={{width:"150px", display:"inline-block"}}><b>Generation:</b></div> {revision.generation}</p> : ""}
+                    {revision.cmd !== "" ?  <p style={{margin:"5px"}}><div style={{width:"150px", display:"inline-block"}}><b>Cmd:</b></div> {revision.cmd}</p> : ""}
+                </div>
+                <div style={{flex: 1, textAlign:"left"}}>
+                    {revision.image !== "" ? <p style={{margin:"5px"}}><div style={{width:"150px", display:"inline-block"}}><b>Image:</b></div> {revision.image}</p> : ""}
+                    {revision.minScale ? <p style={{margin:"5px"}}><div style={{width:"150px", display:"inline-block"}}><b>Scale:</b></div> {revision.minScale}</p> : ""}
+                    {revision.actualReplicas ? <p style={{margin:"5px"}}><div style={{width:"150px", display:"inline-block"}}><b>Actual Replicas:</b></div> {revision.actualReplicas}</p> : ""}
+                    {revision.desiredReplicas ? <p style={{margin:"5px"}}><div style={{width:"150px", display:"inline-block"}}><b>Desired Replicas:</b></div> {revision.desiredReplicas}</p> : ""}
+                </div>
+            </div>
+            <div style={{display:"flex", width:"100%"}}>
+                <div style={{flex: 1, textAlign:"left"}}>
+                    <p style={{margin:"5px"}}><div style={{width:"150px", display:"inline-block"}}><b>Conditions:</b></div></p>
+                    <ul style={{margin:"5px", fontSize:"10pt"}}>
+                        {revision.conditions.map((obj)=>{
+                            let circleFill = "success"
+                            if (obj.status === "False") {
+                                circleFill = "failed"
+                            }
+                            if (obj.status === "Unknown"){
+                                circleFill = "crashed"
+                            }
+                            return(
+                                <li style={{lineHeight:"24px"}}>
+                                    <CircleFill className={circleFill} style={{ paddingTop: "5px", marginRight: "4px", maxHeight: "8px" }} />
+                                    <span style={{fontWeight:500}}>{obj.name}</span> {obj.reason!==""?<i style={{fontSize:"12px"}}>({obj.reason})</i>:""} <span style={{fontSize:'12px'}}>{obj.message}</span>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <div style={{flex: 1, textAlign:"left"}}>
+                    <p style={{margin:"5px"}}><div style={{width:"150px", display:"inline-block"}}><b>Pods:</b></div></p>
+                    <ul style={{margin:"5px", fontSize:"10pt"}}>
+                        {pods.map((obj)=>{
+                            console.log(obj, "pod log")
+                            let circleFill = "crashed"
+                            if (obj.status === "Succeeded" || obj.status === "Running") {
+                                circleFill = "success"
+                            }
+                            if (obj.status === "Failed") {
+                                circleFill = "failed"
+                            }
+                            if (obj.status === "Pending") {
+                                circleFill = "pending"
+                            }
+                            console.log(obj.name, `${serviceName}-deployment-`)
+                            return(
+                                <li style={{lineHeight:"24px"}}>
+                                    <CircleFill className={circleFill} style={{ paddingTop: "5px", marginRight: "4px", maxHeight: "8px" }} />
+                                    <span style={{fontWeight:500}}>{obj.name.replace(`${serviceName}-deployment-`, "")}</span>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
             </div>
         </div>
     )
@@ -152,7 +241,6 @@ function PodLogs(props) {
     // set new log watcher on pod
     useEffect(()=>{
         if(pod !== "") {
-            console.log("creating new event listener for logs")
             let x = `/watch/functions/${service}/revisions/${revision}/pods/${pod}/logs/`
             if (namespace) {
                 x = `/watch/namespaces/${namespace}/functions/${service}/revisions/${revision}/pods/${pod}/logs/`
@@ -189,15 +277,15 @@ function PodLogs(props) {
     useEffect(()=>{
         return () => {
             if (logSource !== null) {
-                console.log('closing log source')
                 logSource.close()
                 setLogs("")
-                document.getElementById("pod-logs").innerHTML = ""
+                if (document.getElementById("pod-logs")) {
+                    document.getElementById("pod-logs").innerHTML = ""
+                }
             }
         }
     },[logSource])
 
-    console.log(logs, "LOGS")
     return(
         <div id="logs-toggle" className="editor-wrapper" style={{display: "flex", flexDirection: "row", flexWrap: "wrap", width: "100%", height: "100%", minHeight:"300px",  top:"-28px", position: "relative", boxShadow:"none"}}>
             <div style={{width: "100%", height: "100%"}}>
