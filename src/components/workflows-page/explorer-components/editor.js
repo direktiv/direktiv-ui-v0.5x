fae83d4ec2378426300210e3b29161c49c9de0c4
Jon Alfaro <jon.alfaro@vorteil.io>
2021-09-21 14:11:53 +1000

    css fixes on workflow page
    Signed-off-by: Jon Alfaro <jon.alfaro@vorteil.io>

--- src/components/workflows-page/explorer-components/editor.js
+++ src/components/workflows-page/explorer-components/editor.js
@@ -8,9 +8,9 @@ export default function EditorDetails(props) {
     return(
         <>
             {editorTab === "editor" ? 
-                        <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", width: "100%", height: "100%", minHeight: "300px", top: "-28px", position: "relative" }}>
-                            <div style={{ width: "100%", height: "100%", position: "relative" }}>
-                                <div style={{ height: "auto", position: "absolute", left: 0, right: 0, top: "25px", bottom: 0 }}>
+                        <div style={{ display: "flex", flexDirection: "column", flexWrap: "wrap", width: "100%", minHeight: "300px", top: "-28px", position: "relative", flex: 1 }}>
+                            <div style={{ width: "100%", flex: "1", height: "100%", position: "relative" }}>
+                                <div style={{ height: "auto", position: "absolute", left: 0, right: 0, top: "25px", bottom: "-25px" }}>
                                     <Editor refValSet={wfRefValue} functions={functions} editorRef={codemirrorRef} err={actionErr} value={workflowValue} setValue={setWorkflowValue} saveCallback={updateWorkflow} showFooter={true} actions={[<LogButton showLogEvent={showLogEvent} updateLogEvent={updateLogEvent} setShowLogEvent={setShowLogEvent} logEvent={logEvent} setLogEvent={setLogEvent} />, <SaveButton workflowValueOld={workflowValueOld} workflowValue={workflowValue} updateWorkflow={updateWorkflow} />]} commentKey={"#"}/>
                                 </div>
                             </div>
